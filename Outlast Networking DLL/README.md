329c1fcf213a0313f55ac12ca28e6149a59f5c98
Lucian <llxlucian1@gmail.com>
2025-02-07 21:13:04 -0500

    remove stuff and stuff

--- Outlast Networking DLL/README.md
+++ Outlast Networking DLL/README.md
@@ -1,3 +1,5 @@
 # Outlast DLL
 
 This is what unreal engine 3 uses in order to use custom mp functions.
+
+(SCRAPPED, PLEASE DONT USE.)
--- Outlast Networking DLLL/Networking.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <boost/asio.hpp>
-#include "pch.h"
-
-extern "C" __declspec(dllexport) bool ConnectToServer(const char* server_ip, int port);
-extern "C" __declspec(dllexport) void SendMessageToServer(const char* message);
-extern "C" __declspec(dllexport) void DisconnectFromServer();
